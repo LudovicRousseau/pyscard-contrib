47df317b38f493b00e0f1885025bdb531892f56a
ludov <ludov@afec0d47-af30-0410-a19f-fe2ccf33c325>
2009-06-12 09:55:47 +0000

    stress test parseATR with the ilist of known ATRs

---
+++ trunk/contrib/parseATR/stress_test.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+"""
+    stress_test.py
+    Copyright (C) 2009   Ludovic Rousseau
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+import sys, string
+import parseATR
+
+List = "/usr/local/share/pcsc/smartcard_list.txt"
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        List = sys.argv[1]
+
+    for atr in open(List):
+        if atr[0] != "3":
+            continue
+        if "[" in atr:
+            continue
+        if "." in atr:
+            continue
+        if "?" in atr:
+            continue
+        print atr
+        parseATR.atr_display_txt(parseATR.parseATR(atr))
