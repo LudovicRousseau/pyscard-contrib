47103ba3ef5b3af6e78566d42b7db7e3c519a9db
jdaussel <jean-daniel.aussel@gemalto.com>
2011-10-16 15:09:16 +0000

    Fixed E261 pep8 at least two spaces before inline comment

--- parseATR/parseATR.py
+++ parseATR/parseATR.py
@@ -625,7 +625,7 @@ def analyse_histrorical_bytes(historical_bytes):
         (lcs, sw1, sw2) = status[:3]
         text.append("    Mandatory status indicator (3 last bytes)\n")
         text.append("      LCS (life card cycle): %d (%s)\n" % (lcs, life_cycle_status(lcs)))
-        text.append("      SW: %02X%02X (%s)" % (sw1, sw2, "")) #Chipcard::PCSC::Card::ISO7816Error("$sw1 $sw2"))
+        text.append("      SW: %02X%02X (%s)" % (sw1, sw2, ""))  # Chipcard::PCSC::Card::ISO7816Error("$sw1 $sw2"))
 
     elif hb_category == 0x80:
         text.append(" (compact TLV data object)\n")
